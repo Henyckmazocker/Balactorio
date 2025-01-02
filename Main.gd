6cbcf2bce65c19d15c6e890e9097ce91612b8048
DESKTOP-PQEKSDR\david <david.carvajal.abellan@gmail.com>
2025-01-02 21:16:28 +0100

    modifying creation of factory from json

--- Main.gd
+++ Main.gd
@@ -2,16 +2,19 @@ extends Node
 
 var factoryArray = [];
 var wood = 0;
-var timer;
+var timer = 0;
+var file = "resources/factoryParams.json"
+var fileData;
 
 @export var factory: PackedScene
 @export var player: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer = 0.0;
 	var player = player.instantiate()
 	add_child(player)
+	var json_as_text = FileAccess.get_file_as_string(file)
+	fileData = JSON.parse_string(json_as_text)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,17 +23,20 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if(get_node("Player").selectedFactory == null):
+				get_node("Player").selectedFactory = get_node("Player").availableFactories[0]
+			var selectedFactoryParams = fileData.Factories[get_node("Player").selectedFactory]
 			var fabrica = factory.instantiate()
+			fabrica.initialize(selectedFactoryParams.material, selectedFactoryParams.tick);
 			fabrica.position = event.position
 			add_child(fabrica)
 			factoryCreated(fabrica);
 
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			print("Wheel up")
-
 func _on_timer_timeout():
+	timer += 1;
 	for x in factoryArray:
-		get_node("Player").get_node("Bag").addToBag(x.type, 1)
+		if int(timer) % int(x.tickTimer) == 0:
+			get_node("Player").get_node("Bag").addToBag(x.type, 1)
 
 func factoryCreated(node):
 	factoryArray.append(node);
--- factoryData.gd
+++ factoryData.gd
@@ -2,12 +2,15 @@ extends Area2D
 
 var tickTimer;
 var type;
+var deliversTo;
+var recievesFrom;
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	tickTimer = 1;
-	type = "wood";
-
+	pass
+	
+func initialize(name, tick):
+	tickTimer = tick;
+	type = name;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
